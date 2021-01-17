d418aa5559096ad24a0a973aa28f1221b9854236
Marc Sànchez <msanxes@gmail.com>
2021-01-17 15:01:32 +0100

    [FEAT] Created resolver for collectibles, mutation first iteration

--- resolvers/Collection.js
+++ resolvers/Collection.js
@@ -1,9 +1,15 @@
-import DataAccess from '../access'
+import DataAccess from '../dataSources'
 
 const Collections = Object.keys(DataAccess.collectibles).map(key => {return {name: DataAccess.collectibles[key].name, collectibleList: DataAccess.collectibles[key].collectibleList}})
 
+
 const CollectionResolver = {
-  Query: { collections: () =>  Collections}
+  Query: { collections: () =>  Collections},
+  // Mutation: {
+  //   increaseCollectible: async (_, {collectionName, collectibleName}, {dataSources}) => {
+
+  //   }
+  // }
 }
 
 export default CollectionResolver
